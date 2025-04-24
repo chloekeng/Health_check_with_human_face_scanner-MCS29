719054dbea0a1910aea6e4ef3f70a65e2feb027e
noodnoof (nadia) <nadianafeesa@gmail.com>
2025-04-24 20:15:40 +1000

    added tracker file and code to organise dataset
    - Documentation to track which sets and quantity of faces are reserved for training and validation set
    - Code to rename bulk images for better organisation

---
+++ rename.py
@@ -0,0 +1,32 @@
+import os
+
+folder_path = r"C:\Users\User\OneDrive\Desktop\deep_learning_acute_illness-main\data\unparsed\sick_ai"
+
+# Get all files in the folder
+files = sorted(os.listdir(folder_path))  # Sorting ensures consistency
+
+# Loop through files and rename them
+for index, filename in enumerate(files, start=1):
+    old_path = os.path.join(folder_path, filename)
+    
+    # Extract file extension
+    # file_extension = os.path.splitext(filename)[1]  # Includes the dot (e.g., .jpg, .png)
+    file_extension = ".jpg"
+
+    # # Extract whole file name
+    filename = os.path.splitext(filename)[0]  # Excludes the dot (e.g., .jpg, .png)
+
+    # Create new filename in the format "s1-m", "s2-m", etc.
+    new_filename = f"ai-{index}-sick{file_extension}"
+
+    # # add on the word "new_" to the start of the filename
+    # new_filename = f"new_{filename}{file_extension}"
+
+    new_path = os.path.join(folder_path, new_filename)
+
+
+    # Rename the file
+    os.rename(old_path, new_path)
+    print(f"Renamed: {filename} → {new_filename}")
+
+print("✅ Done renaming all files!")
