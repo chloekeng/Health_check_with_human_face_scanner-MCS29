82fecff1361218e85066bd88513965dd291ee9da
noodnoof (nadia) <nadianafeesa@gmail.com>
2025-05-01 04:23:28 +1000

    trained #6 and added plots and updated documentation

--- rename.py
+++ rename.py
@@ -17,10 +17,12 @@ for index, filename in enumerate(files, start=1):
     filename = os.path.splitext(filename)[0]  # Excludes the dot (e.g., .jpg, .png)
 
     # Create new filename in the format "s1-m", "s2-m", etc.
-    new_filename = f"h-{index}-b2{file_extension}"
+    new_filename = f"h-{index+91}-b3{file_extension}"
 
     # for me to prepend "rug"
     # new_filename = f"rug_{filename}{file_extension}" 
+    # new_filename = f"ai{filename}{file_extension}" 
+
 
     # # add on the word "new_" to the start of the filename
     # new_filename = f"new_{filename}{file_extension}"
